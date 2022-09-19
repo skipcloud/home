46dad2829f7bdd7d2e95dfa82cb832b4ed57235c
Skip Gibson <skipcloudgibson@gmail.com>
2022-09-19 14:47:29 +0100

    netatmo: move all auth related code to module

--- netatmo/auth.rb
+++ netatmo/auth.rb
@@ -5,27 +5,34 @@ module Netatmo
   module Auth
     CLIENT_ID_ENV_VAR = 'NETATMO_CLIENT_ID'
     CLIENT_SECRET_ENV_VAR = 'NETATMO_CLIENT_SECRET'
-    OAUTH_TOKEN_ENDPOINT = 'https://api.netatmo.com/oauth2/token'
+    OAUTH_TOKEN_ENDPOINT = '/oauth2/token'
     NETATMO_PASSWORD_ENV_VAR = 'NETATMO_PASSWORD'
+
+    def self.included(_)
+      throw StandardError, 'Client secrets not found' unless \
+        ENV[CLIENT_SECRET_ENV_VAR] && \
+        ENV[CLIENT_ID_ENV_VAR] && \
+        ENV[NETATMO_PASSWORD_ENV_VAR]
+    end
+
+    def authenticate
+      resp = request_token
+      # TODO: check status
+
+      @access_token = resp.body['access_token']
+      @refresh_token = resp.body['refresh_token']
+    end
 
     private
 
-    def env_vars_set?
-      ENV[CLIENT_SECRET_ENV_VAR] && ENV[CLIENT_ID_ENV_VAR] && ENV[NETATMO_PASSWORD_ENV_VAR]
-    end
-
     def request_token
-      Faraday.post(
+      @conn.post(
         OAUTH_TOKEN_ENDPOINT,
-        data,
-        {
-          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-          'Host': 'api.netatmo.com'
-        }
+        request_token_data
       )
     end
 
-    def data
+    def request_token_data
       {
         client_id: ENV[CLIENT_ID_ENV_VAR],
         client_secret: ENV[CLIENT_SECRET_ENV_VAR],
@@ -33,6 +40,12 @@ module Netatmo
         username: 'skip.gibson@hey.com',
         password: ENV[NETATMO_PASSWORD_ENV_VAR],
         scope: 'read_station'
+      }
+    end
+
+    def refresh_token
+      {
+
       }
     end
   end
--- netatmo/client.rb
+++ netatmo/client.rb
@@ -1,19 +1,28 @@
 # frozen_string_literal: true
 
+require_relative './auth'
 require 'faraday'
+require 'faraday/retry'
 
 module Netatmo
   # A Netatmo Client class that can interact with
   # the netatmo API to pull weather station data
   class Client
+    include Auth
     NETATMO_API_URL = 'https://api.netatmo.com'
 
     def initialize
-      throw StandardError, 'Client secrets not found' unless env_vars_set?
+      @conn = Faraday.new(NETATMO_API_URL) do |c|
+        c.request :url_encoded
+        c.request :retry
+        c.response :json
+      end
+
+      authenticate
     end
 
     def fetch_data
-      puts request_token.body
+      puts @access_token
     end
   end
 end
